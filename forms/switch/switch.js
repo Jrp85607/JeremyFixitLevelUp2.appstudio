bb4f1d19f958fc0c78a3070a3e9873050426f12b
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-09-20 12:00:19 -0500

    Updated

--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -16,19 +16,19 @@ let tempS = Number(prompt('Enter the temperature in Farenheit'));
 
 // output based on the user input
 switch(true) {
-  case (stateS.toUpperCase() == stateNameS[0] && tempS < 32):
-    NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[0]}`)
-    break
-  case (stateS.toUpperCase() == stateNameS[0] && 32 <= tempS && tempS <= 50):
-   NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[1]}`)
-    break
-  case (stateS.toUpperCase() == stateNameS[1] && 50 <= tempS && tempS <= 70):
-    NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[2]}`)
-    break
-  case (stateS.toUpperCase() == stateNameS[1] && 32 <= tempS && tempS < 50):
-    NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[3]}`)
-    break
-  default:
-    NSB.MsgBox(messagesS[4])
+case (stateS.toUpperCase() == stateNameS[0] && tempS < 32):
+  NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[0]}`)
+  break
+case (stateS.toUpperCase() == stateNameS[0] && 32 <= tempS && tempS <= 50):
+ NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[1]}`)
+  break
+case (stateS.toUpperCase() == stateNameS[1] && 50 <= tempS && tempS <= 70):
+  NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[2]}`)
+  break
+case (stateS.toUpperCase() == stateNameS[1] && 32 <= tempS && tempS < 50):
+  NSB.MsgBox(`${updatedNameS}, you are from ${stateS.toUpperCase()} \and it \is currently ${tempS} degrees, ${messagesS[3]}`)
+  break
+default:
+  NSB.MsgBox(messagesS[4])
 }
 
